3143d43ec8e036a20483fa1d55e678303311e61e
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-27 23:04:59 +0530

    two setTimeout method runned

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,10 +1,12 @@
 console.log('Starting')
 
 setTimeout(()=>{
-
     console.log('two seconds timer')
-
 },2000)
+
+setTimeout(()=>{
+    console.log('zero seconds timer')
+},0)
 
 
 console.log('Stopping')
