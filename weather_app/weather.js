d4f47eae82e3039d1f5836b93f5fccb174e100e4
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-28 18:20:44 +0530

    data showed separately

--- weather_app/weather.js
+++ weather_app/weather.js
@@ -1,11 +1,12 @@
 const request = require('request')
 
-const url = 'http://api.weatherapi.com/v1/current.json?key=9d0967b05e6048f6b7b122823202808&q=London'
+const url = 'http://api.weatherapi.com/v1/current.json?key=9d0967b05e6048f6b7b122823202808&q=India'
 
-request({url: url},(error,response)=>{
-    const data = JSON.parse(response.body)
-    console.log(data.location)
-    console.log(data.current)
+request({url: url,json: true},(error,response)=>{
+    //const data = JSON.parse(response.body)
+    //console.log(response.body.location)
+    console.log('The name '+ response.body.location.name+ ' and the country is '+ response.body.location.country)
+    //console.log(data.current)
     
     //console.log(response)
 })
