3e0b61f9cd53c454e523f9a3aa698c89c88bc73b
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-29 19:44:56 +0530

    first callback function challenge code

--- playground/callbacks.js
+++ playground/callbacks.js
@@ -1,26 +1,38 @@
-setTimeout(()=>{
-    console.log('2 seconds are up')
-},2000)
+// setTimeout(()=>{
+//     console.log('2 seconds are up')
+// },2000)
 
-const names = ['Harsh','Gupta','Sparsh','Shivam']
+// const names = ['Harsh','Gupta','Sparsh','Shivam']
 
-const shortnames = names.filter((name)=>{
-    return name.length <=5
-})
+// const shortnames = names.filter((name)=>{
+//     return name.length <=5
+// })
 
-console.log(shortnames)
+// console.log(shortnames)
 
 
-const geocode = (address,callback)=>{
+// const geocode = (address,callback)=>{
+//     setTimeout(()=>{
+//         const data = {
+//             latitude: 0,
+//             longitute: 0
+//         }
+//         callback(data)
+//     },2000)  
+// }
+
+// geocode('delhi',(data)=>{
+//     console.log(data)
+// })
+
+
+const add = (value1, value2, callback)=>{
     setTimeout(()=>{
-        const data = {
-            latitude: 0,
-            longitute: 0
-        }
-        callback(data)
-    },2000)  
+        callback(value1+value2)
+    },2000)
 }
 
-geocode('delhi',(data)=>{
-    console.log(data)
+
+add(1,4,(sum)=>{
+    console.log(sum)
 })
