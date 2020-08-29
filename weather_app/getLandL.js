93d7196f314eddcedaa92c18fdb25ac2f23fc1b1
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-29 20:33:16 +0530

    get Latitude and Longitude done with callback function

--- weather_app/getLandL.js
+++ weather_app/getLandL.js
@@ -1,28 +1,54 @@
 const request = require('request')
 
-const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/12max.json?limit=1&access_token=pk.eyJ1IjoiaGFyc2g4MDciLCJhIjoiY2tlZWE0MDR3MGVoZzJxbjdlaGhjZ2ZoMCJ9.AIL-3P7lFbQZ3iSFDLvKkw'
+// const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/12max.json?limit=1&access_token=pk.eyJ1IjoiaGFyc2g4MDciLCJhIjoiY2tlZWE0MDR3MGVoZzJxbjdlaGhjZ2ZoMCJ9.AIL-3P7lFbQZ3iSFDLvKkw'
 
-request({url: url,json: true},(error,response)=>{
+// request({url: url,json: true},(error,response)=>{
 
-    if(error){
-        console.log('Unable to get the Latitude and longitude service!')
-    }else if(response.body.message){
-        console.log(response.body.message)
-    }else if(response.body.features.length === 0){
-        console.log('There is no such place exist!!')
-    }else{
-        console.log(response.body.features[0].center[0])
-        console.log(response.body.features[0].center[1])
-    }
+//     if(error){
+//         console.log('Unable to get the Latitude and longitude service!')
+//     }else if(response.body.message){
+//         console.log(response.body.message)
+//     }else if(response.body.features.length === 0){
+//         console.log('There is no such place exist!!')
+//     }else{
+//         console.log(response.body.features[0].center[0])
+//         console.log(response.body.features[0].center[1])
+//     }
 
-    //console.log(error)
+//     //console.log(error)
 
-    //const data = JSON.parse(response.body)
-    // console.log(response.body.features[0].center[0])
-    // console.log(response.body.features[0].center[1])
-    //console.log(response.body.features[0].properties.wikidata)
-    //console.log('The name '+ response.body.location.name+ ' and the country is '+ response.body.location.country)
-    //console.log(data.current)
+//     //const data = JSON.parse(response.body)
+//     // console.log(response.body.features[0].center[0])
+//     // console.log(response.body.features[0].center[1])
+//     //console.log(response.body.features[0].properties.wikidata)
+//     //console.log('The name '+ response.body.location.name+ ' and the country is '+ response.body.location.country)
+//     //console.log(data.current)
     
-    //console.log(response)
+//     //console.log(response)
+// })
+
+
+
+const getLatituteandLongitute = (address,callback) => {
+    
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address)+ '.json?limit=1&access_token=pk.eyJ1IjoiaGFyc2g4MDciLCJhIjoiY2tlZWE0MDR3MGVoZzJxbjdlaGhjZ2ZoMCJ9.AIL-3P7lFbQZ3iSFDLvKkw'
+
+    request({url: url , json: true},(error, response)=>{
+        if(error){
+            callback('Unable to connect to get Latitute and Longitute service!',undefined)
+        }else if(response.body.features.length === 0){
+            callback('Unable to find the Latitude and Longitute of this address!',undefined)
+        }else{
+            callback(undefined,{
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+getLatituteandLongitute('New York',(error, data)=>{
+    console.log('Error',error)
+    console.log('Data', data)
 })
