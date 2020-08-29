7cd6c4871e4c7e2690d1eae5a9302aee31a94708
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-30 00:16:49 +0530

    address input from command line argument customized

--- weather_app/app.js
+++ weather_app/app.js
@@ -17,22 +17,27 @@ const getweatherinfo = require('./utils/weather')
 const address = process.argv[2]
 console.log(address)
 
-getLatiandLong(address,(error, data)=>{
-    if(!address){
-        return console.log('parameter is required')
-    }else if(error){
-        return console.log(error)
-    }    
-    getweatherinfo(data.latitude,data.longitude,(error,getweatherdata)=>{
-       if(error){
-           return console.log(error)
-       }
-       console.log('Name',getweatherdata.name)
-       console.log('Region',getweatherdata.region)
-       console.log('Country',getweatherdata.country)
-       console.log('Temp in F',getweatherdata.tempinf)
-       console.log('Temp in C',getweatherdata.tempinc)
+if(!address){
+    console.log('parameter is required')
+}else{
+    getLatiandLong(address,(error, data)=>{
+        if(error){
+            return console.log(error)
+        }    
+        getweatherinfo(data.latitude,data.longitude,(error,getweatherdata)=>{
+           if(error){
+               return console.log(error)
+           }
+           console.log('Name',getweatherdata.name)
+           console.log('Region',getweatherdata.region)
+           console.log('Country',getweatherdata.country)
+           console.log('Temp in F',getweatherdata.tempinf)
+           console.log('Temp in C',getweatherdata.tempinc)
+        })
+    
     })
+}
 
-})
+
+
 
