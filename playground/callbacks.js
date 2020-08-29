596156cb750d7717263011288778daed1bab4354
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-29 19:29:09 +0530

    call back function returning problem

--- playground/callbacks.js
+++ playground/callbacks.js
@@ -1,3 +1,24 @@
 setTimeout(()=>{
     console.log('2 seconds are up')
 },2000)
+
+const names = ['Harsh','Gupta','Sparsh','Shivam']
+
+const shortnames = names.filter((name)=>{
+    return name.length <=5
+})
+
+console.log(shortnames)
+
+
+const geocode = (address,callback)=>{
+    setTimeout(()=>{
+        const data = {
+            latitude: 0,
+            longitute: 0
+        }
+    },2000)  
+}
+
+const data = geocode('delhi')
+console.log(data)
