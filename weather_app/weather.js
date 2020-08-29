78d2c11f7771ae20260c572a6329130f2cb07479
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-29 15:01:21 +0530

    error handling with is-elseif-else

--- weather_app/weather.js
+++ weather_app/weather.js
@@ -1,12 +1,14 @@
 const request = require('request')
 
-const url = 'http://api.weaatherapi.com/v1/current.json?key=9d0967b05e6048f6b7b122823202808&q=37.8267,37.8267'
+const url = 'http://api.weatherapi.com/v1/current.json?key=9d0967b05e6048f6b7b122823202808&q=37.8267,37.8267'
 
 request({url: url,json: true},(error,response)=>{
     
     if (error) {
         console.log('Unable to connect to weather!')
-    } else {
+    }else if(response.body.error){
+        console.log(response.body.error.message)
+    }else {
         console.log('The name '+ response.body.location.name+ ' and the country is '+ response.body.location.country)
     }
 
