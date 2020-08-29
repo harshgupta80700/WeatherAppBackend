882a30ba25934b4d122f9120a357920d3d732982
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-29 14:51:55 +0530

    error handling with if-else

--- weather_app/weather.js
+++ weather_app/weather.js
@@ -3,7 +3,16 @@ const request = require('request')
 const url = 'http://api.weaatherapi.com/v1/current.json?key=9d0967b05e6048f6b7b122823202808&q=37.8267,37.8267'
 
 request({url: url,json: true},(error,response)=>{
-    console.log(error)
+    
+    if (error) {
+        console.log('Unable to connect to weather!')
+    } else {
+        console.log('The name '+ response.body.location.name+ ' and the country is '+ response.body.location.country)
+    }
+
+
+
+
     //const data = JSON.parse(response.body)
     //console.log(response.body)
     //console.log('The name '+ response.body.location.name+ ' and the country is '+ response.body.location.country)
