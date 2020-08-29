6abf93dfa0a93d22945c72857cb81372102b3fe9
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-29 23:27:11 +0530

    get lat and long function using module.exports

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,12 +1,19 @@
-console.log('Starting')
+// console.log('Starting')
 
-setTimeout(()=>{
-    console.log('two seconds timer')
-},2000)
+// setTimeout(()=>{
+//     console.log('two seconds timer')
+// },2000)
 
-setTimeout(()=>{
-    console.log('zero seconds timer')
-},0)
+// setTimeout(()=>{
+//     console.log('zero seconds timer')
+// },0)
 
 
-console.log('Stopping')
+// console.log('Stopping')
+
+const getLatiandLong = require('./utils/getLandL')
+
+getLatiandLong('Pune',(error, data)=>{
+    console.log('Error',error)
+    console.log('Data', data)
+})
--- weather_app/weather.js
+++ weather_app/weather.js
@@ -11,14 +11,26 @@ request({url: url,json: true},(error,response)=>{
     }else {
         console.log('The name '+ response.body.location.name+ ' and the country is '+ response.body.location.country)
     }
+})
 
+const getweatherinfo = (latitute, longitude, callback)=>{
+    const url  = 'http://api.weatherapi.com/v1/current.json?key=9d0967b05e6048f6b7b122823202808&q='+ latitute + ',' + longitude
 
+    request({url: url,json:true},(error,response)=>{
+        if(error){
+            callback('Unable to connect with the get weather service!',undefined)
+        }else if(response.body.error){
+            callback(response.body.error.message,undefined)
+        }else{
+            callback(undefined,{
+                
+            })
+        }
+    })
 
+}
 
-    //const data = JSON.parse(response.body)
-    //console.log(response.body)
-    //console.log('The name '+ response.body.location.name+ ' and the country is '+ response.body.location.country)
-    //console.log(data.current)
-    
-    //console.log(response)
+getweatherinfo(18.52361,73.84778,(error,data)=>{
+    console.log('Error',error)
+    console.log('Data',data)
 })
