2b57802417617172fd8d7822332c7ace5bcf0343
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-30 23:38:58 +0530

    4 routes made

--- web_servers/src/app.js
+++ web_servers/src/app.js
@@ -2,10 +2,20 @@ const express = require('express')
 
 const app = express()
 
-app.get('',(req , res)=>{
-    
+app.get('',(req , res)=>{    
     res.send('Hello Express!')
+})
 
+app.get('/help',(req,res)=>{
+    res.send('This is the Help Page')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('this is the about page')
+})
+
+app.get('/weather',(req,res)=>{
+    res.send('This is the weather app')
 })
 
 app.listen(3000,()=>{
