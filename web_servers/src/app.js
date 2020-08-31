0ceba75afa7d5d1078d60ce24772ee6988ea7628
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-01 01:01:05 +0530

    index.hbs file uploded to the browser

--- web_servers/src/app.js
+++ web_servers/src/app.js
@@ -7,8 +7,13 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
 
+app.set('view engine','hbs')
+
 
 app.use(express.static(publicDirectoryPath))
+app.get('',(req,res)=>{
+    res.render('index')
+})
 
 // app.get('',(req , res)=>{    
 //     res.send('<h1>Hello Express!</h1>')
