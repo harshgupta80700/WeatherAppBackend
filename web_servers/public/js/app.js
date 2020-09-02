25af852293ad6c07c7594c05dd17a6bfabbd1a07
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-02 20:02:11 +0530

    weather details shown to the UI

--- web_servers/public/js/app.js
+++ web_servers/public/js/app.js
@@ -9,18 +9,28 @@ fetch('https://puzzle.mead.io/puzzle').then((response) => {
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 =  document.querySelector('#message-2')
+
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    message1.textContent = ''
+    message2.textContent = ''
     const location = search.value
+    message1.textContent = 'Loading...'
     fetch('http://localhost:3000/weather?address='+location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log('Invalid address location')
+                //console.log('Invalid address location')
                 console.log(data.error)
+                message1.textContent = data.error
             } else {
                 console.log(data.location)
                 console.log(data.tempinC)
+                message1.textContent = data.location
+                message2.textContent = data.tempinC
             }
         })
     })
