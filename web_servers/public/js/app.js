752a813972b8a5a690c2add7a5cf64a6aab9b1bd
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-02 16:59:33 +0530

    data shown to browser inspect using fetch

--- web_servers/public/js/app.js
+++ web_servers/public/js/app.js
@@ -1,1 +1,7 @@
 console.log('Client Side JavaScript is loaded into the Page!')
+
+fetch('https://puzzle.mead.io/puzzle').then((response)=>{
+    response.json().then((data)=>{
+        console.log(data)
+    })
+})
