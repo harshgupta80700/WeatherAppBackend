36a895888dceee350a1128e5d6b7ba832b421019
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-02 19:46:02 +0530

    weather details shown in the console from input feild

--- web_servers/public/js/app.js
+++ web_servers/public/js/app.js
@@ -1,28 +1,27 @@
 console.log('Client Side JavaScript is loaded into the Page!')
 
-fetch('https://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
+fetch('https://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
         console.log(data)
     })
 })
 
-fetch('http://localhost:3000/weather?address=!').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log('Invalid address location')
-            console.log(data.error)
-        }else{
-            console.log(data.location)
-            console.log(data.tempinC)
-        }
-    })
-})
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
-weatherForm.addEventListener('submit',(e)=>{
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
-    console.log(location)
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log('Invalid address location')
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.tempinC)
+            }
+        })
+    })
 })
